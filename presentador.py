3c656d58a125ffeed35d3159d70a50d4d5cc26ae
piipeesteban94 <72636710+piipeesteban94@users.noreply.github.com>
2024-05-20 21:43:39 -0400

    se creo un ejercicio de inputs

---
+++ presentador.py
@@ -0,0 +1,14 @@
+nombre = input('Ingrese su Nombre: ')
+edad = input('Ingrese Edad: ')
+ocupacion = input('Ingrese Ocupación: ')
+lugar = input('En dónde?: ')
+caracteristica_1 = input('Ingrese 3 características:\n1. > ')
+caracteristica_2 = input('2. > ')
+caracteristica_3 = input('3. > ')
+pasatiempo = input('Cuál es tu pasatiempo: ')
+hacer = input('¿Que quieres hacer? ')
+
+print(f'''
+Mi nombre es {nombre}, tengo {edad} años y me desempeño como {ocupacion} en {lugar}.
+Soy una persona {caracteristica_1}, {caracteristica_2} y {caracteristica_3}.
+Mi pasatiempo es {pasatiempo} y me gustaría poder {hacer}.''')
